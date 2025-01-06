bf22e8fd88661344147f7fe89078521e50b28da9
jacky michy <jacky.michy@tellent.com>
2025-01-06 19:59:05 +0100

    feat(app): install NX, api, front

---
+++ apps/front/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'front',
+  preset: '../../jest.preset.cjs',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/front',
+}
