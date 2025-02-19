7fcd451e85720d3db39da70a5e6976b3d3e9b9bd
jacky michy <jacky.michy@tellent.com>
2025-02-19 16:35:34 +0100

    fix(ci): build only app for production

--- apps/front/next.config.ts
+++ apps/front/next.config.ts
@@ -6,8 +6,6 @@ import { NextConfig } from 'next'
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
-const API_URL =
-  process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_API_URL : ''
 
 const nextConfig: NextConfig = {
   output: 'standalone',
@@ -22,14 +20,6 @@ const nextConfig: NextConfig = {
       use: ['@svgr/webpack'],
     })
     return config
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${API_URL}/:path*`,
-      },
-    ]
   },
 }
 
