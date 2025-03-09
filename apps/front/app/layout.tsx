5b1085dc7f118a961fb95d22032db9b6130e815e
jacmanh <jacky.michy@gmail.com>
2025-03-09 18:30:05 +0100

    fix(analytics): Suspense with useSearchParams

--- apps/front/app/layout.tsx
+++ apps/front/app/layout.tsx
@@ -1,7 +1,7 @@
 import { ToastProvider } from '@app/front/provider/ToastProvider'
 import React from 'react'
-import ReactQueryProvider from '../provider/ReactQueryProvider'
 import { GoogleAnalytics } from '../components/GoogleAnalytics'
+import ReactQueryProvider from '../provider/ReactQueryProvider'
 
 export const metadata = {
   title: "Taskly'n Hutch",
@@ -9,20 +9,15 @@ export const metadata = {
     'This project is an experiment in combining technology, creativity, and structure to simulate the development of a startup.',
 }
 
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ReactQueryProvider>
       <html lang="en">
-        <head>
-          <GoogleAnalytics />
-        </head>
+        <head />
         <body>
           <ToastProvider>{children}</ToastProvider>
         </body>
+        <GoogleAnalytics />
       </html>
     </ReactQueryProvider>
   )
